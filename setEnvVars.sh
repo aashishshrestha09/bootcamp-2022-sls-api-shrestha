fe0f87b60a864e6fb9d3b401f17432d2d4195e8d
Aashish Shrestha <ashrestha@dminc.com>
2022-07-01 14:23:42 -0400

    Initial commit

---
+++ setEnvVars.sh
@@ -0,0 +1,3 @@
+export GITHUB_USERNAME="aashishshrestha09"
+export GITHUB_REPO="bootcamp-2022-sls-api-shrestha"
+export BUCKET_NAME="aashishshrestha09-bootcamp-2022-bucket" #username must be all lowercase
